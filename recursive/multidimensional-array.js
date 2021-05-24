1c2894d9a0b7eeeb1005f99843879255e6a54e05
ali akbar hakim <osairis.ali@gmail.com>
2021-05-24 08:47:06 +0700

    trying to implement deep copy of object

--- recursive/multidimensional-array.js
+++ recursive/multidimensional-array.js
@@ -34,3 +34,45 @@ console.log("mat3By3: ", mat3By3);
 const filledMat = fillJaggedArray(mat3By3, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 console.log("filled matrix: ", filledMat);
 console.log("mat3By3: ", mat3By3); // kenapa ikut berubah???
+
+const obj = {
+  name: "ali",
+  detail: {
+    home: "TPJ",
+    phone: [0215, 3256, [1], [1, [2, [3]]]],
+    occupation: "web developer",
+    skill: { "data strutures": "best", algorithm: "best", coding: "average" },
+  },
+  realtive: "none",
+};
+
+const cloneArray = (array) => {
+  return array.map((el) => (Array.isArray(el) ? cloneArray(el) : el));
+};
+
+const clonedArray = cloneArray(obj.detail.phone);
+clonedArray[1] = 23232323232323232323;
+console.log(obj.detail.phone);
+console.log(clonedArray);
+
+const deepClone = (obj, temp = {}, tempArr = []) => {
+  for (let prop in obj) {
+    if (typeof obj[prop] === "object" && !Array.isArray(obj[prop])) {
+      temp[prop] = deepClone(obj[prop]);
+    }
+    if (Array.isArray(obj[prop])) {
+      tempArr.push(cloneArray(obj[prop]));
+      temp[prop] = tempArr;
+    }
+    temp[prop] = obj[prop];
+  }
+  return temp;
+};
+
+const clonedObj = deepClone(obj);
+console.log("deepCloned: ", clonedObj);
+clonedObj["name"] = "akbar";
+console.log("obj: ", obj);
+console.log("clonedObj: ", clonedObj);
+console.log(Array.from(clonedObj.detail.phone))
+console.log(cloneArray(clonedObj.detail.phone))
